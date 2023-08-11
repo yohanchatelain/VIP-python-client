def6d969467821d43eccde638ff19dad39afeb14
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-08-11 10:55:12 -0400

    Add longitidunal script

---
+++ example/freesurfer-fuzzy/run-longitudinal.sh
@@ -0,0 +1,29 @@
+# Run from rep<n> directory
+
+mkdir -p base_template
+export SUBJECTS_DIR=$(pwd) FS_LICENSE=$(realpath ../../../../license.txt)
+
+# Step 1 - base template
+# Data from json_data_base.json
+# Freesurfer Zenodo 7916240
+# Create an unbiased template from all time points for each subject and process it with recon-all
+FIRST_VISIT=sub-101070_ses-BL
+SECOND_VISIT=sub-101070_ses-V04
+BASE=sub-101070
+singularity exec -B $(realpath ../../../..):$(realpath ../../../..) $(realpath ~/lustre/freesurfer.sif) recon-all -base base_template/${BASE} -tp ${FIRST_VISIT} -tp ${SECOND_VISIT} -all
+
+# Step 2 - longitudinally processed timepoints
+# Data from json_data_long.json
+# Freesurfer Zenodo 7920788
+# "-long" longitudinally process all timepoints (recon-all -long)
+VISIT=sub-101070_ses-BL
+BASE=sub-101070
+singularity exec -B $(realpath ../../../..):$(realpath ../../../..) $(realpath ~/lustre/freesurfer.sif) recon-all -long -tp ${VISIT} base_template/${BASE} -all
+
+# Step 3 - Qcache
+# Data from json_data_long.json
+# Freesurfer Zenodo 7920876
+# save proprocessing script to submit jobs to the server later
+VISIT=sub-101070_ses-BL
+BASE=sub-101070
+singularity exec -B $(realpath ../../../..):$(realpath ../../../..) $(realpath ~/lustre/freesurfer.sif) recon-all -long -tp ${VISIT} base_template/${BASE} -qcache

3075abdf321f103f655625318e3a20bc939c3a38
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-11-09 15:25:03 -0500

    Add check-fail-run.py script

--- example/freesurfer-fuzzy/scripts/check-fail-run.py
+++ example/freesurfer-fuzzy/scripts/check-fail-run.py
@@ -1,40 +1,53 @@
+import tarfile
 import os
+import argparse
 import glob
 
-
-def get_logs():
-    logs = {}
-    logs_files = glob.glob("rep*/*/scripts/recon-all-.log")
-    for log in logs_files:
-        rep = os.path.split(log)[0]
-        logs[rep] = logs.get(rep, []) + [log]
-    return logs
+# Define the path to the .tgz archive and the file to check within the archive
+archive_path = "path/to/archive.tgz"
+file_to_check = "scripts/recon-all.done"
 
 
-def get_last_line(filename):
-    with open(filename, "rb") as file:
-        try:
-            file.seek(-2, os.SEEK_END)
-            while file.read(1) != b"\n":
-                file.seek(-2, os.SEEK_CUR)
-        except OSError:
-            file.seek(0)
-        last_line = file.readline().decode()
-    return last_line
+def get_archives(directory):
+    archives = glob.glob(os.path.join(directory, "*.tgz"))
+    return archives
 
 
-def parse_log(logs):
-    for rep, log_list in logs.items():
-        print(rep)
-        for log in log_list:
-            last_line = get_last_line(log)
-            if "finished without error" not in last_line:
-                print(log)
+# Function to check if a file exists in a tar.gz archive
+def check_file_in_tgz(archive_path, file_to_check):
+    try:
+        # Open the tar.gz archive
+        with tarfile.open(archive_path, "r:gz") as archive:
+            # List all members of the archive
+            archive_members = [os.path.basename(f) for f in archive.getnames()]
+            # Check if the specified file exists in the list
+            if file_to_check in archive_members:
+                return True
+            else:
+                return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("check-fail-run")
+    parser.add_argument(
+        "--directory",
+        required=True,
+        help="Path to the directory containing the .tgz archive",
+    )
+    args = parse_args()
+    return args
 
 
 def main():
-    logs = get_logs()
-    parse_log(logs)
+    args = parse_args()
+    # Call the function and print the result
+    archives = get_archives(args.directory)
+    for archive_path in archives:
+        if not check_file_in_tgz(archive_path, file_to_check):
+            print(archive_path, "does not contain", file_to_check)
 
 
 if __name__ == "__main__":

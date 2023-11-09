f1f0547e646e6ca3115627d0d835d3bc9ba48118
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-11-09 15:59:50 -0500

    Skip subject that does not have stat files

--- example/freesurfer-fuzzy/scripts/compute-aseg_aparc-aggregation.sh
+++ example/freesurfer-fuzzy/scripts/compute-aseg_aparc-aggregation.sh
@@ -104,12 +104,12 @@ for rep in rep*/; do
     for meas in "${MEASUREMENTS[@]}"; do
         # Handle subcortical volume separately as it does not require hemispheric specification
         if [ "$meas" == "volume" ]; then
-            python3 $asegstats2table --subjects ${SUBJECTS[@]} --meas "$meas" --tablefile "$OUTPUT_DIR/${rep}.aseg.$meas.tsv"
+            python3 $asegstats2table --skip --subjects ${SUBJECTS[@]} --meas "$meas" --tablefile "$OUTPUT_DIR/${rep}.aseg.$meas.tsv"
             echo "aseg $meas table created at $OUTPUT_DIR/aseg.$meas.tsv"
         else
             # Loop through hemispheres for cortical measurements
             for hemi in lh rh; do
-                python3 $aparcstats2table --parc aparc.a2009s --hemi "$hemi" --subjects ${SUBJECTS[@]} --meas "$meas" --tablefile "$OUTPUT_DIR/${rep}.${hemi}.aparc.$meas.tsv"
+                python3 $aparcstats2table --skip --parc aparc.a2009s --hemi "$hemi" --subjects ${SUBJECTS[@]} --meas "$meas" --tablefile "$OUTPUT_DIR/${rep}.${hemi}.aparc.$meas.tsv"
                 echo "${hemi} hemisphere aparc $meas table created at $OUTPUT_DIR/${hemi}.aparc.$meas.tsv"
             done
         fi

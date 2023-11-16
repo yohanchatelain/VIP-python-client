6d296d4842849aa4c04c58b6ebe1374e9cc0320a
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-11-16 16:30:45 -0500

    Update check-fail-run

--- example/freesurfer-fuzzy/scripts/check-fail-run.py
+++ example/freesurfer-fuzzy/scripts/check-fail-run.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import glob
 import tqdm
+import pathlib
 
 # Define the path to the .tgz archive and the file to check within the archive
 file_to_check = "recon-all.done"
@@ -11,6 +12,11 @@ file_to_check = "recon-all.done"
 def get_archives(directory):
     archives = glob.glob(os.path.join(directory, "*.tgz"))
     return archives
+
+
+def get_directory(directory):
+    directories = glob.glob(os.path.join(directory, "sub-*"))
+    return directories
 
 
 # Function to check if a file exists in a tar.gz archive
@@ -30,12 +36,30 @@ def check_file_in_tgz(archive_path, file_to_check):
         return False
 
 
+def has_unzip_directory(unzip_directory, archive_path):
+    unzip_directory = os.path.join(unzip_directory, os.path.basename(archive_path))
+    return os.path.isdir(unzip_directory)
+
+
+def check_file_in_directory(unzip_directory, file_to_check):
+    # iterate over the directory and check if the file exists
+    for _, _, files in os.walk(unzip_directory):
+        if file_to_check in files:
+            return True
+    return False
+
+
 def parse_args():
     parser = argparse.ArgumentParser("check-fail-run")
     parser.add_argument(
         "--directory",
         required=True,
         help="Path to the directory containing the .tgz archive",
+    )
+    parser.add_argument(
+        "--unzip-directory",
+        required=True,
+        help="Path to the directory where to unzip the archive",
     )
     parser.add_argument("--file-to-check", default=file_to_check)
     args = parser.parse_args()
@@ -48,7 +72,10 @@ def main():
     archives = get_archives(args.directory)
     failed = []
     for archive_path in tqdm.tqdm(archives):
-        if not check_file_in_tgz(archive_path, file_to_check):
+        if has_unzip_directory(args.unzip_directory, archive_path):
+            if not check_file_in_directory(args.unzip_directory, file_to_check):
+                failed.append(archive_path)
+        elif not check_file_in_tgz(archive_path, file_to_check):
             failed.append(archive_path)
 
     for archive_path in failed:
--- example/freesurfer-fuzzy/scripts/run-base-template.sh
+++ example/freesurfer-fuzzy/scripts/run-base-template.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export FS_LICENSE=$HOME/projects/rrg-glatard/ychatel/living-park/VIP-python-client/example/freesurfer-fuzzy/license.txt
+
 # Run from rep<n> directory
 
 FIRST_VISIT=$1
@@ -7,10 +9,10 @@ SECOND_VISIT=$2
 BASE=$3
 
 mkdir -p base_template
-export SUBJECTS_DIR=$(pwd) FS_LICENSE=$(realpath ../../../../license.txt)
+export SUBJECTS_DIR=$(pwd)
 
 # Step 1 - base template
 # Data from json_data_base.json
 # Freesurfer Zenodo 7916240
 # Create an unbiased template from all time points for each subject and process it with recon-all
-singularity exec -B $(realpath ../../../..):$(realpath ../../../..) $(realpath ~/lustre/freesurfer.sif) recon-all -base base_template/${BASE} -tp ${FIRST_VISIT} -tp ${SECOND_VISIT} -all
+apptainer exec -B $(realpath ../../../..):$(realpath ../../../..) $(realpath ~/lustre/freesurfer.sif) recon-all -base base_template/${BASE} -tp ${FIRST_VISIT} -tp ${SECOND_VISIT} -all

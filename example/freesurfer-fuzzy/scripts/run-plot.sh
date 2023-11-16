9fc4cfe2e55d63cee740a68a714a30f0736b9d84
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-11-16 13:54:38 -0500

    Add plot_stats.py

--- example/freesurfer-fuzzy/scripts/run-plot.sh
+++ example/freesurfer-fuzzy/scripts/run-plot.sh
@@ -3,144 +3,88 @@
 ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 
 SCRIPT=${ROOT}/plot_stats.py
-DIRECTORY=${ROOT}/csv
+DIRECTORY=${ROOT}/../csv
 
 function basename_wo_ext() {
     echo $(basename $1) | cut -d'.' -f1
 }
 
-# Significant digits
-xaxis="ROI"
-yaxis="Significant digits"
+function plot() {
+    filename=$1
+    title=$2
+    region=$3
+    output=$(basename_wo_ext $filename)
+    echo "Parse" $filename
+    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
+        --analysis-level ${level} --region="cortical" \
+        --xaxis "${xaxis}" --yaxis "${yaxis}" \
+        --title "${title}" 2>>log &
+}
+
+get_title() {
+    case "$1" in
+    "area") echo "Surface area" ;;
+    "thickness") echo "Cortical thickness" ;;
+    "volume") echo "Cortical volume" ;;
+    "subcortical-volume") echo "Subcortical volume" ;;
+    *) echo "Unknown title" ;;
+    esac
+}
+
+get_region() {
+    case "$1" in
+    "area") echo "cortical" ;;
+    "thickness") echo "cortical" ;;
+    "volume") echo "cortical" ;;
+    "subcortical-volume") echo "subcortical" ;;
+    *) echo "Unknown region" ;;
+    esac
+}
 
 rm -f log
 
+# Significant digits
+xaxis="ROI"
+yaxis="Significant digits"
 for level in "subject" "group"; do
-
-    filename=${DIRECTORY}/${level}-area-sig.csv
-    output=$(basename_wo_ext $filename)
-    title="Surface area"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
-    filename=${DIRECTORY}/${level}-thickness-sig.csv
-    output=$(basename_wo_ext $filename)
-    title="Cortical thickness"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
-    filename=${DIRECTORY}/${level}-volume-sig.csv
-    output=$(basename_wo_ext $filename)
-    title="Cortical volume"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
-    filename=${DIRECTORY}/${level}-subcortical-volume-sig.csv
-    output=$(basename_wo_ext $filename)
-    title="Subcortical volume"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="subcortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
+    for datatype in "area" "thickness" "volume" "subcortical-volume"; do
+        filename="${DIRECTORY}/${level}-${datatype}-sig.csv"
+        title=$(get_title "$datatype")
+        region=$(get_region "$datatype")
+        plot "$filename" "$title" "$region"
+    done
 done
 
 # Standard deviation
 xaxis="ROI"
 yaxis="Standard deviation (log)"
-
 for level in "subject" "group"; do
-
-    filename=${DIRECTORY}/${level}-area-std.csv
-    output=$(basename_wo_ext $filename)
-    title="Surface area"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" --log-yaxis 2>>log
-
-    filename=${DIRECTORY}/${level}-thickness-std.csv
-    output=$(basename_wo_ext $filename)
-    title="Cortical thickness"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" --log-yaxis 2>>log
-
-    filename=${DIRECTORY}/${level}-volume-std.csv
-    output=$(basename_wo_ext $filename)
-    title="Cortical volume"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" --log-yaxis 2>>log
-
-    filename=${DIRECTORY}/${level}-subcortical-volume-std.csv
-    output=$(basename_wo_ext $filename)
-    title="Subcortical volume"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="subcortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" --log-yaxis 2>>log
-
+    for datatype in "area" "thickness" "volume" "subcortical-volume"; do
+        filename="${DIRECTORY}/${level}-${datatype}-std.csv"
+        title=$(get_title "$datatype")
+        region=$(get_region "$datatype")
+        plot "$filename" "$title" "$region"
+    done
 done
 
 # Mean
 xaxis="ROI"
 yaxis="Mean"
-
 for level in "subject" "group"; do
-
-    filename=${DIRECTORY}/${level}-area-mean.csv
-    output=$(basename_wo_ext $filename)
-    title="Surface area"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
-    filename=${DIRECTORY}/${level}-thickness-mean.csv
-    output=$(basename_wo_ext $filename)
-    title="Cortical thickness"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
-    filename=${DIRECTORY}/${level}-volume-mean.csv
-    output=$(basename_wo_ext $filename)
-    title="Cortical volume"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="cortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
-    filename=${DIRECTORY}/${level}-subcortical-volume-mean.csv
-    output=$(basename_wo_ext $filename)
-    title="Subcortical volume"
-    echo "Parse" $filename
-    python3 ${SCRIPT} --mca-filename ${filename} --output ${output} \
-        --analysis-level ${level} --region="subcortical" \
-        --xaxis "${xaxis}" --yaxis "${yaxis}" \
-        --title "${title}" 2>>log
-
+    for datatype in "area" "thickness" "volume" "subcortical-volume"; do
+        filename="${DIRECTORY}/${level}-${datatype}-mean.csv"
+        title=$(get_title "$datatype")
+        region=$(get_region "$datatype")
+        plot "$filename" "$title" "$region"
+    done
 done
 
+wait
+
 cat log
+
+mkdir -p figures/{group,subject}/{png,html}
+mv subject-*.png figures/subject/png
+mv group-*.png figures/group/png
+mv subject-*.html figures/subject/html
+mv group-*.html figures/group/html

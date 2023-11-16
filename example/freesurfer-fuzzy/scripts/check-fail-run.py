d83c4e2111504693245001e1974a4b7c10e3a7df
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-11-16 18:12:48 -0500

    Extract file

--- example/freesurfer-fuzzy/scripts/check-fail-run.py
+++ example/freesurfer-fuzzy/scripts/check-fail-run.py
@@ -2,11 +2,9 @@ import tarfile
 import os
 import argparse
 import glob
-import tqdm
-import logging
+from joblib import Parallel, delayed
 
 
-logger = logging.getLogger(__name__)
 # Define the path to the .tgz archive and the file to check within the archive
 file_to_check = "recon-all.done"
 
@@ -29,15 +27,7 @@ def check_file_in_tgz(archive_path, unzip_directory, file_to_check):
             # List all members of the archive
             archive_members = [os.path.basename(f) for f in archive.getnames()]
             # Check if the specified file exists in the list
-            print(archive_members)
-            if file_to_check in archive_members:
-                subject = os.path.splitext(os.path.basename(archive_path))[0]
-                unzip_directory_dest = os.path.join(unzip_directory, subject)
-                print(f"extracting {file_to_check} to: ", unzip_directory_dest)
-                archive.extract(file_to_check, path=unzip_directory_dest)
-                return True
-            else:
-                return False
+            return file_to_check in archive_members
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
@@ -53,7 +43,6 @@ def extract_file_from_tgz(archive_path, unzip_directory, file_to_check):
                 [file_to_check_fullpath] = [
                     f for f in archive_names if f.endswith(file_to_check)
                 ]
-                print(f"extract {file_to_check_fullpath} to: ", unzip_directory)
                 archive.extract(file_to_check_fullpath, path=unzip_directory)
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -61,11 +50,8 @@ def extract_file_from_tgz(archive_path, unzip_directory, file_to_check):
 
 
 def has_unzip_directory(unzip_directory, archive_path):
-    print("unzip_directory: ", unzip_directory)
-    print("archive_path: ", archive_path)
     subject = os.path.splitext(os.path.basename(archive_path))[0]
     unzip_directory = os.path.join(unzip_directory, subject)
-    print("unzip_directory full path: ", unzip_directory)
     return os.path.isdir(unzip_directory)
 
 
@@ -77,6 +63,20 @@ def check_file_in_directory(archive_path, unzip_directory, file_to_check):
         if file_to_check in files:
             return True
     return False
+
+
+def check_failure(archive_path, args):
+    if has_unzip_directory(args.unzip_directory, archive_path):
+        if not check_file_in_directory(
+            archive_path, args.unzip_directory, file_to_check
+        ):
+            extract_file_from_tgz(archive_path, args.unzip_directory, file_to_check)
+        if not check_file_in_directory(
+            archive_path, args.unzip_directory, file_to_check
+        ):
+            return archive_path
+    elif not check_file_in_tgz(archive_path, args.unzip_directory, file_to_check):
+        return archive_path
 
 
 def parse_args():
@@ -93,37 +93,19 @@ def parse_args():
     )
     parser.add_argument("--verbose", action="store_true")
     parser.add_argument("--file-to-check", default=file_to_check)
+    parser.add_argument("--n-jobs", type=int, default=-1)
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    logger.setLevel(logging.DEBUG)
-    if args.verbose:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-
     # Call the function and print the result
     archives = get_archives(args.directory)
-    failed = []
-    for archive_path in tqdm.tqdm(archives):
-        if has_unzip_directory(args.unzip_directory, archive_path):
-            print("has unzip directory")
-            if not check_file_in_directory(
-                archive_path, args.unzip_directory, file_to_check
-            ):
-                print(f"{file_to_check} not in {args.unzip_directory}")
-                print("extract it")
-                extract_file_from_tgz(archive_path, args.unzip_directory, file_to_check)
-            if not check_file_in_directory(
-                archive_path, args.unzip_directory, file_to_check
-            ):
-                print(f"Failed: {file_to_check} not in {args.unzip_directory}")
-                failed.append(archive_path)
-        elif not check_file_in_tgz(archive_path, args.unzip_directory, file_to_check):
-            failed.append(archive_path)
+    res = Parallel(n_jobs=args.n_jobs, verbose=10 if args.verbose else 0)(
+        delayed(check_failure)(archive_path) for archive_path in archives
+    )
+    failed, i = zip(*res)
 
     for archive_path in failed:
         print(archive_path, "does not contain", file_to_check)

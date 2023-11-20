696922b4b5c546386845167a8305e0d808d0565e
yohanchatelain <yohan.chatelain@gmail.com>
2023-11-20 13:43:51 -0500

    Update base template.sh

--- example/freesurfer-fuzzy/scripts/run-base-template.sh
+++ example/freesurfer-fuzzy/scripts/run-base-template.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-export FS_LICENSE=$HOME/projects/rrg-glatard/ychatel/living-park/VIP-python-client/example/freesurfer-fuzzy/license.txt
+PROJECT_ROOT=$HOME/projects/rrg-glatard/ychatel/living-park/VIP-python-client/
+FS_SIF=$PROJECT_ROOT/freesurfer-7.3.1.sif
+
+export FS_LICENSE=$PROJECT_ROOT/freesurfer-fuzzy/license.txt
 
 # Run from rep<n> directory
+REPETITION=$1
+FIRST_VISIT=$2
+SECOND_VISIT=$3
+BASE=$4
 
-FIRST_VISIT=$1
-SECOND_VISIT=$2
-BASE=$3
+cd /scratch/ychatel/living-park/VIP-python-client/example/freesurfer-fuzzy/${REPETITION}
 
 mkdir -p base_template
 export SUBJECTS_DIR=$(pwd)
+
 
 # Step 1 - base template
 # Data from json_data_base.json
 # Freesurfer Zenodo 7916240
 # Create an unbiased template from all time points for each subject and process it with recon-all
-apptainer exec -B $(realpath ../../../..):$(realpath ../../../..) $(realpath ~/lustre/freesurfer.sif) recon-all -base base_template/${BASE} -tp ${FIRST_VISIT} -tp ${SECOND_VISIT} -all
+apptainer exec -B $(realpath ../../../..):$(realpath ../../../..) ${FS_SIF} recon-all -base base_template/${BASE} -tp ${FIRST_VISIT} -tp ${SECOND_VISIT} -all

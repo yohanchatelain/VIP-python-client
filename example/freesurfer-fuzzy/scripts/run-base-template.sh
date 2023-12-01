3e95a8fe301ba1c0e9cc14176ad462837bb5f279
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-11-30 23:49:21 -0500

    Modify run-base-template.sh to remove online extraction

--- example/freesurfer-fuzzy/scripts/run-base-template.sh
+++ example/freesurfer-fuzzy/scripts/run-base-template.sh
@@ -1,90 +1,59 @@
 #!/bin/bash
 
-set -xe
+#set -xe
 
-# Job id of the current task
-# SLURM_JOB_ID=36
-# Job id corresponding to the first job of the array
-# SLURM_ARRAY_JOB_ID=36 (37,38)
-# Index of the current task
-# SLURM_ARRAY_TASK_ID=1
-# Number of tasks in the array
-# SLURM_ARRAY_TASK_COUNT=3
-# Highest index of the array
-# SLURM_ARRAY_TASK_MAX=3
-# Lowest index of the array
-# SLURM_ARRAY_TASK_MIN=1
+#SBATCH --job-name=run-base-template
+#SBATCH --array=1-13
+#SBATCH --output=run-base-template_%A_%a.out
+#SBATCH --error=run-base-template_%A_%a.err
+#SBATCH --time=72:00:00
+#SBATCH --ntasks=256
+#SBATCH --threads-per-core=1
+#SBATCH --mem-per-cpu=16GB
 
-if [ -z "${SLURM_ARRAY_TASK_ID}" ]; then
-    # Simulate a slurm job array with one task
-    SLURM_ARRAY_TASK_COUNT=1
-    SLURM_ARRAY_TASK_ID=1
-fi
-REPETITION=$SLURM_ARRAY_TASK_ID
-
-if [ -z "${SLURM_NTASKS}" ]; then
-    # Simulate a slurm job array with one task
-    SLURM_NTASKS=1
-fi
-
-# Initialize a variable to indicate whether --dry-run is set to false by default
-dry_run=false
-
-# Check if the --dry-run argument is passed
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-    --dry-run)
-        dry_run=true
-        shift # Remove the argument from the list of arguments
-        ;;
-    --input-json)
-        INPUT_JSON="$2"
-        shift # Remove the argument from the list of arguments
-        shift # Remove the value from the list of arguments
-        ;;
-    --n-tasks)
-        SLURM_NTASKS="$2"
-        shift # Remove the argument from the list of arguments
-        shift # Remove the value from the list of arguments
-        ;;
-    *)
-        # Handle other arguments here if needed
-        shift # Remove the argument from the list of arguments
-        ;;
-    esac
-done
-
-# Check the value of dry_run
-if [ "$dry_run" = true ]; then
-    DRY_RUN="--dry-run"
-else
-    DRY_RUN=""
-fi
+REPETITION=rep${SLURM_ARRAY_TASK_ID}
 
 PROJECT_ROOT=$HOME/projects/rrg-glatard/ychatel/living-park/VIP-python-client/
-ARCHIVE_PATH=$PROJECT_ROOT/example/freesurfer-fuzzy/vip_outputs/freesurfer-fuzzy
-PYTHON_SCRIPT=$PROJECT_ROOT/example/freesurfer-fuzzy/scripts/run-base-template.py
+SUBJECT_DIRS=$PROJECT_ROOT/example/freesurfer-fuzzy/vip_outputs/freesurfer-fuzzy/${REPETITION}
 FS_SIF=$PROJECT_ROOT/freesurfer-7.3.1.sif
+INPUT_JSON=$PROJECT_ROOT/example/freesurfer-fuzzy/scripts/json_data_base.json
 
-if [ -z "$INPUT_JSON" ]; then
-    INPUT_JSON=$PROJECT_ROOT/example/freesurfer-fuzzy/scripts/json_data_base.json
-fi
+cd $SUBJECT_DIRS
 
-OUTPUT_DIR=/scratch/ychatel/VIP-python-client/example/freesurfer-fuzzy/vip_outputs/freesurfer-fuzzy/rep${REPETITION}
+DST_LICENSE_DIR=/etc/license
+DST_LICENSE_PATH=$DST_LICENSE_DIR/license.txt
+DST_HOME_DIR=/home/
+DST_SUBJECT_DIRS=$DST_HOME_DIR
+SRC_HOME_DIR=$SUBJECT_DIRS
+SRC_LICENSE_DIR=${PROJECT_ROOT}
 
-# Run from rep<n> directory
-cd ${OUTPUT_DIR}
+# Read the keys (assuming all dictionaries have the same keys)
+PATNO=$(jq -r '(.first_visit | keys[])' "$INPUT_JSON")
 
-# Step 1 - base template
-# Data from json_data_base.json
-# Freesurfer Zenodo 7916240
-# Create an unbiased template from all time points for each subject and process it with recon-all
-python3 $PYTHON_SCRIPT --fs-image ${FS_SIF} \
-    --input ${INPUT_JSON} \
-    --repetition ${REPETITION} \
-    --archive-dir ${ARCHIVE_PATH}/rep${REPETITION} \
-    --output-dir ${OUTPUT_DIR} \
-    --src-license-dir ${PROJECT_ROOT} \
-    --src-home ${PWD} \
-    --n-jobs ${SLURM_NTASKS} \
-    ${DRY_RUN}
+COUNTER=1
+
+rm -f to_run
+
+# Iterate over the keys
+for patno in $PATNO; do
+    echo -ne "\r${COUNTER}/${#PATNO[@]}"
+    ((COUNTER++))
+    # Extract corresponding values from each dictionary
+    first_visit=$(jq -r ".first_visit[\"$patno\"]" "$INPUT_JSON")
+    second_visit=$(jq -r ".second_visit[\"$patno\"]" "$INPUT_JSON")
+    PATNO_base=$(jq -r ".PATNO_base[\"$patno\"]" "$INPUT_JSON")
+
+        # Check if both files exist
+    if [[ -d "$first_visit" && -d "$second_visit" ]]; then
+        echo "apptainer -B ${SRC_LICENSE_DIR}:${DST_LICENSE_DIR} -B ${SRC_HOME_DIR}:${DST_HOME_DIR}  --env SUBJECT_DIRS=${DST_SUBJECT_DIRS} --env FS_LICENSE=${DST_LICENSE_PATH} ${FS_SIF} recon-all -all -base ${PATNO_base} -tp ${first_visit} -tp ${second_visit}" >> to_run
+    else
+        if [[ ! -d "$first_visit" ]]; then
+            echo "Error: $PWD/$first_visit is missing"
+        fi
+        if [[ ! -d "$second_visit" ]]; then
+            echo "Error: $PWD/$second_visit is missing"
+        fi
+    fi
+done
+
+cat to_run | parallel --max-procs=128 '{}'
